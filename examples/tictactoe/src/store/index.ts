e7670895f95a48f26e7cc4658914cc087f408429
Mauro <mauro@eijsenring.com>
2023-08-18 13:40:21 +0200

    Update for tictactoe example project (#19)

--- examples/tictactoe/src/store/index.ts
+++ examples/tictactoe/src/store/index.ts
@@ -1,42 +1,24 @@
-import { GameBoard, Player } from "../types";
-import { createZKState } from "zk-states";
-import { create } from "zustand";
 import { produce } from "immer";
+import { createZKState } from "zk-states";
+import type { GameBoard, Player } from "../types";
 
-export interface TicTacToeStore {
+const EMPTY_BOARD = [{ squares: new Array<Player>(9).fill("") }];
+
+// ZK store for the TicTacToe game these values generate a proof on state change
+interface ZKState {
+  board: Player[];
   history: GameBoard[];
   turnNumber: number;
   xIsNext: boolean;
   finished: boolean;
 
+  updateBoard: (index: number, value: Player) => void;
+  setBoard: (board: Player[]) => void;
   newTurn: (turnNumber: number) => void;
   clearHistory: () => void;
   setXIsNext: () => void;
   setFinished: (finished: boolean) => void;
   setHistory: (history: GameBoard[]) => void;
-}
-
-const EMPTY_BOARD = [{ squares: new Array<Player>(9).fill("") }];
-
-// Zustand store for the TicTacToe game genereates no proof on state change
-export const useStore = create<TicTacToeStore>()((set) => ({
-  history: EMPTY_BOARD,
-  turnNumber: 0,
-  xIsNext: true,
-  finished: false,
-
-  newTurn: (turnNumber) => set(() => ({ turnNumber: turnNumber })),
-  setHistory: (history) => set(() => ({ history: history })),
-  clearHistory: () => set(() => ({ history: EMPTY_BOARD })),
-  setXIsNext: () => set((state) => ({ xIsNext: state.turnNumber % 2 === 0 })),
-  setFinished: (finished) => set(() => ({ finished: finished })),
-}));
-
-// ZK store for the TicTacToe game these values generate a proof on state change
-interface ZKState {
-  board: Player[];
-  updateBoard: (index: number, value: Player) => void;
-  setBoard: (board: Player[]) => void;
 }
 
 export const { useInitZkStore, useZKStore, useGetLatestProof } =
@@ -46,6 +28,17 @@ export const { useInitZkStore, useZKStore, useGetLatestProof } =
     }),
     (set) => ({
       board: new Array<Player>(9).fill(""),
+      history: EMPTY_BOARD,
+      turnNumber: 0,
+      xIsNext: true,
+      finished: false,
+
+      newTurn: (turnNumber) => set(() => ({ turnNumber: turnNumber })),
+      setHistory: (history) => set(() => ({ history: history })),
+      clearHistory: () => set(() => ({ history: EMPTY_BOARD })),
+      setXIsNext: () =>
+        set((state) => ({ xIsNext: state.turnNumber % 2 === 0 })),
+      setFinished: (finished) => set(() => ({ finished: finished })),
       updateBoard: (index, value) => {
         set(
           produce((state: ZKState) => {
@@ -55,4 +48,5 @@ export const { useInitZkStore, useZKStore, useGetLatestProof } =
       },
       setBoard: (board) => set(() => ({ board: board })),
     }),
+    ["board"],
   );
